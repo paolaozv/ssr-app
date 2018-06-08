7d1f95351c05ff68222f04a26f1a3dc63f061e4e
Paola Ortiz de Zevallos Villanueva <paolaozv@gmail.com>
2018-06-08 17:04:01 -0500

    Add webpack client

--- server/src/client/components/Home.js
+++ server/src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Home Component</div>
-}
+  return (
+    <div>
+      <div>Home Component</div>
+      <button onClick={() => console.log('Hi there!')}>Press me!</button>
+    </div>
+  );
+};
 
 export default Home;
