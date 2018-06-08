a27c1c6627a28c7d1962983f6325ea7da1d12690
Paola Ortiz de Zevallos Villanueva <paolaozv@gmail.com>
2018-06-08 12:18:33 -0500

    Add webpack file configuration and add Readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# Server Side Rendering React App
+
+It's a server side rendering react application
+
+### Build bundle.js
+
+```
+npm run dev:build:server
+```
+
+### Start server
+
+```
+node build/bundle.js
+```
--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const React = require('react');
+const renderToString = require('react-dom/server').renderToString;
+const Home = require('./client/components/Home').default;
 const app = express();
 
-app.get('/', (req, res) => {});
+app.get('/', (req, res) => {
+  const content = renderToString(<Home />);
+  
+  res.send(content);
+});
 
 const PORT = 3000;
 
